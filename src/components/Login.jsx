664a4add4d7d5cca27247bf811e93b263a74736c
Towhidul1999 <towhidulsislam@gmail.com>
2022-08-03 19:14:49 +0600

    registration/login page design

---
+++ src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="card-header">
+                    <h3>Login</h3>
+                </div>
+                <div className="card-body">
+                    <form action="">
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" placeholder="Email" id='email' className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="Password" id="password" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" value="Login" className="btn"/>
+                        </div>
+                        <div className="form-group">
+                            <span><Link to="/messenger/register">Register your Account</Link></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
